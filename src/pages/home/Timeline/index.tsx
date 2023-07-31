2f68ae6c4783919de6ac8b37b0c35be3693c87f0
Samuelwsz <Samuelxwsz@gmail.com>
2023-07-30 22:34:16 -0300

    att

--- src/pages/home/Timeline/index.tsx
+++ src/pages/home/Timeline/index.tsx
@@ -1,20 +1,35 @@
+import { FormEvent, useState } from "react"
 import Cabecalho from "../../../components/Cabecalho"
 import Separador from "../../../components/Separador"
 import Tweet from "../../../components/Tweets"
 import styles from "./Timeline.module.css"
 
-const Tweets = ["tweet 1", "tweet 2 ", "tweet 3"]
+let newTweet = ""
 
 export default function Timelime() {
+  const [tweets, setTweets] = useState(["tweet 1", "tweet 2 ", "tweet 3"])
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault()
+
+    setTweets([newTweet, ...tweets])
+  }
+
   return (
     <>
       <main className={styles.timeline}>
         <Cabecalho title="Home" />
 
-        <form className={styles.new_tweet_form}>
+        <form className={styles.new_tweet_form} onSubmit={createNewTweet}>
           <label htmlFor="tweet">
             <img src="https://github.com/samuelwsz.png" alt="Samuel" />
-            <textarea id="tweet" placeholder="What´s happening" />
+            <textarea
+              id="tweet"
+              placeholder="What´s happening"
+              onChange={(event) => {
+                newTweet = event.target.value
+              }}
+            />
           </label>
 
           <button type="submit">Tweet</button>
@@ -22,7 +37,7 @@ export default function Timelime() {
 
         <Separador />
 
-        {Tweets.map((tweet, index) => {
+        {tweets.map((tweet, index) => {
           return <Tweet key={index} content={tweet} />
         })}
       </main>
